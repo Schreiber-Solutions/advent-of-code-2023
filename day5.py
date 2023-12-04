c1a57d27b7ade077772cf2d5bbe5cf92d7595974
Dan Schreiber <dmschreiber@gmail.com>
2023-12-04 08:28:41 -0500

    prep for Day 5

---
+++ day5.py
@@ -0,0 +1,39 @@
+import re
+import scrib
+import os
+from collections import namedtuple
+
+
+def part2(input):
+    with open(input) as f:
+        input_lines = f.read().splitlines()
+
+    total = 0
+
+    return total
+
+
+def part1(input):
+    with open(input) as f:
+        input_lines = f.read().splitlines()
+
+    total = 0
+
+    return total
+
+
+if __name__ == '__main__':
+    d = scrib.find_filename(__file__)
+    d = d[:len(d)-3]
+
+    input_file = "./data/" + d + "_input.txt"
+    print("{} part 1: {}".format(d,part1(input_file)))
+    print("{} part 2: {}".format(d,part2(input_file)))
+    # print("day 8 part 1: {}".format(part1("./data/day10_test.txt")))
+
+    # lst = [1, 4, 4, 4, 2, 5, 6, 6, 7, 8, 9, 10]
+    # print(scrib.find_most_frequent(lst))
+    # print(scrib.find_occurances(lst)[4])
+    # print(scrib.find_even(lst))
+    # print(scrib.capitalize_words(["python", "javaScript", "c++"]))
+    # print(scrib.reverse_list(lst))
