eef3301bcade6f04cc73fb9fc7a4b75f6f78eb57
Dan Schreiber <dmschreiber@gmail.com>
2023-12-06 08:13:23 -0500

    day 6

--- day6.py
+++ day6.py
@@ -7,7 +7,26 @@ def part2(input):
     with open(input) as f:
         input_lines = f.read().splitlines()
 
+    with open(input) as f:
+        input_lines = f.read().splitlines()
+
     total = 0
+
+    time = int("".join([n for n in input_lines[0].split(":")[1].split()]))
+    distance = int("".join([n for n in input_lines[1].split(":")[1].split()]))
+
+    total = 1
+
+    t = time
+    won = 0
+    for hold_time in range(t+1):
+        traveled = hold_time*(t-hold_time)
+        if traveled > distance:
+            won = won + 1
+
+        # if hold_time/100000 == int(hold_time/100000):
+        #     print("at hold time {}".format(hold_time))
+    total = total * won
 
     return total
 
@@ -17,6 +36,21 @@ def part1(input):
         input_lines = f.read().splitlines()
 
     total = 0
+
+    times = [int(n) for n in input_lines[0].split(":")[1].split()]
+    distances = [int(n) for n in input_lines[1].split(":")[1].split()]
+
+    print(times)
+    total = 1
+    for index in range(len(times)):
+        t = times[index]
+        won = 0
+        for hold_time in range(t+1):
+            traveled = hold_time*(t-hold_time)
+            if traveled > distances[index]:
+                won = won + 1
+        print("{} wins {} times".format(index,won))
+        total = total * won
 
     return total
 
