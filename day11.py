01ca3d2b28f38568865273563fb5908ceccd6b2d
Dan Schreiber <dmschreiber@gmail.com>
2023-12-11 01:13:07 -0500

    Day 11 simplify

--- day11.py
+++ day11.py
@@ -25,8 +25,10 @@ def solve(input):
         for g2 in g:
             if g1 != g2:
                 if (g1,g2) not in d_p1.keys() and (g2,g1) not in d_p1.keys():
-                    d_p2[(g1,g2)] = abs(g1[0]-g2[0]) + abs(g1[1]-g2[1]) + sum([999999 for r in ex_r if min(g1[0],g2[0]) < r < max(g1[0],g2[0])]) + sum([999999 for c in ex_c if min(g1[1],g2[1]) < c < max(g1[1],g2[1])])
-                    d_p1[(g1,g2)] = abs(g1[0]-g2[0]) + abs(g1[1]-g2[1]) + sum([1 for r in ex_r if min(g1[0],g2[0]) < r < max(g1[0],g2[0])]) + sum([1 for c in ex_c if min(g1[1],g2[1]) < c < max(g1[1],g2[1])])
+                    exp_r = (sum([1 for r in ex_r if r in range(*sorted([g1[0],g2[0]]))]))
+                    exp_c = (sum([1 for c in ex_c if c in range(*sorted([g1[1],g2[1]]))]))
+                    d_p2[(g1,g2)] = abs(g1[0]-g2[0]) + abs(g1[1]-g2[1]) + exp_r*999999 + exp_c*999999
+                    d_p1[(g1,g2)] = abs(g1[0]-g2[0]) + abs(g1[1]-g2[1]) + exp_r + exp_c
 
     return sum(d_p1.values()), sum(d_p2.values())
 
@@ -37,6 +39,8 @@ if __name__ == '__main__':
 
     input_file = "./data/" + d + "_input.txt"
     p1, p2 = solve(input_file)
+    assert(p1==9370588)
+    assert(p2==746207878188)
     print("{} part 1: {}".format(d,p1))
     print("{} part 2: {}".format(d,p2))
     # print("day 8 part 1: {}".format(part1("./data/day10_test.txt")))
