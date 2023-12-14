fa27cff6dba2acb6ef35e2e06087b7fc79c61a9a
Dan Schreiber <dmschreiber@gmail.com>
2023-12-14 01:08:48 -0500

    Day 14

--- day14.py
+++ day14.py
@@ -11,19 +11,93 @@ def solve(input):
     g = lines
     p1, p2 = 0, 0
 
-    r = tilt_north_south(lines, True)
+    r = lines
+
+    cache = []
+    possible = []
+
+    for i in range(200):
+        r = tilt_north_south(r, False)
+        if p1 == 0:
+            p1 = sum([(len(r) - i) * len([e for e in r[i] if e == "O"]) for i in range(len(r))])
+
+        r = tilt_east_west(r, False)
+
+        r = tilt_north_south(r, True)
+
+        r = tilt_east_west(r, True)
+        # print_display(r)
+        w = sum([(len(r) - i) * len([e for e in r[i] if e == "O"]) for i in range(len(r))])
+        if w in cache:
+            possible.append(i)
+        cache.append(w)
+
+    possible = [possible[i] for i in range(len(possible)-1) if possible[i+1]==possible[i]+1]
+
+    start = possible[0]
+    possible = [cache[p] for p in possible]
+    pattern = []
+    for k in range(1,len(possible)+1):
+        if possible[:k]*2 == possible[:2*k]:
+            pattern = possible[:k]
+            break
+
+    p2 = pattern[(1000000000 - 1 - start) % len(pattern)]
+
+    # p2 = [69, 69, 65, 64, 65, 63, 68][999971990 % 7]
+    # p2 =  [90212,
+    #          90217,
+    #          90213,
+    #          90200,
+    #          90188,
+    #          90177,
+    #          90176,
+    #          90182,
+    #          90198,
+    #          90211,
+    #          90218,
+    #          90212,
+    #          90201,
+    #          90187,
+    #          90178,
+    #          90175,
+    #          90183,
+    #          90197][(1000000000 - 1 - 363) % 18]
+
+    return p1, p2
+
+
+
+def print_display(r):
     for l in r:
         print("".join(l))
+    print()
 
-    p1 = sum([(len(r)-i)*len([e for e in r[i] if e == "O"]) for i in range(len(r))])
-    len(r)
-    return p1, p2
+
+def tilt_east_west(lines, is_east):
+    rows = lines
+    if is_east:
+        rows = [s.reverse_list(l) for l in rows]
+
+    new_rows = []
+    for r in rows:
+        chunks = str(r).split("#")
+        new_chunks = []
+        for chunk in chunks:
+            n = "".join(['O' for a in chunk if a == 'O']) + "".join(['.' for a in chunk if a == '.'])
+            new_chunks.append(n)
+        new_rows.append("#".join(new_chunks))
+
+    if is_east:
+        new_rows = [s.reverse_list(l) for l in new_rows]
+
+    return new_rows
 
 
 def tilt_north_south(lines, is_south):
     cols = list(zip(*lines))
     if is_south:
-        lines = [s.reverse_list(l) for l in lines]
+        cols = [s.reverse_list(l) for l in cols]
 
     new_cols = []
     for c in cols:
@@ -34,6 +108,10 @@ def tilt_north_south(lines, is_south):
             new_chunks.append(n)
         # print("#".join(new_chunks))
         new_cols.append("#".join(new_chunks))
+
+    if is_south:
+        new_cols = [s.reverse_list(l) for l in new_cols]
+
     r = list(zip(*new_cols))
     return r
 
