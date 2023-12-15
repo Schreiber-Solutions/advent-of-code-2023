b8650dfc17a768dd38e8b60def7a69df006ac6f6
Dan Schreiber <dmschreiber@gmail.com>
2023-12-15 00:47:43 -0500

    Day 15

--- day15.py
+++ day15.py
@@ -10,17 +10,64 @@ def solve(input):
 
     p1, p2 = 0, 0
 
-    return p1, p2
+    lines = lines[0].split(",")
+
+    v = 0
+
+    for l in lines:
+        v = v + my_hash(l)
+
+    box = [[] for b in range(256)]
+
+    cache = {}
+    for l in lines:
+        label = re.search(r"(\w+)", l)
+        label = label[0]
+        op = re.search(r"[-|=]", l)
+        op = op[0]
+        # print(op)
+        if op == "=":
+            n = s.find_int(l)
+        else:
+            n = 0
+
+        h = my_hash(str(label))
+        if op[0] == "-":
+            if label in box[h]:
+                box[h].remove(label)
+
+        if op[0] == "=":
+            if str(label) in box[h]:
+                cache[str(label)] = n
+            else:
+                box[h].append(label)
+                cache[str(label)] = n
+
+    p2 = 0
+    for i, b in enumerate(box):
+        for k, lens in enumerate(b):
+            p2 = p2 + (i + 1) * (k + 1) * cache[lens]
+
+    return v, p2
+
+
+def my_hash(l):
+    v = 0
+    for c in l:
+        v = v + ord(c)
+        v = v * 17
+        v = v % 256
+    return v
 
 
 if __name__ == '__main__':
     d = s.find_filename(__file__)
-    d = d[:len(d)-3]
+    d = d[:len(d) - 3]
 
     input_file = "./data/" + d + "_input.txt"
     p1, p2 = solve(input_file)
-    print("{} part 1: {}".format(d,p1))
-    print("{} part 2: {}".format(d,p2))
+    print("{} part 1: {}".format(d, p1))
+    print("{} part 2: {}".format(d, p2))
 
     # lst = [1, 4, 4, 4, 2, 5, 6, 6, 7, 8, 9, 10]
     # print(s.find_most_frequent(lst))
