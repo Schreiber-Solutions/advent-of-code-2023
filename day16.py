f20f70b8e5de8bd596aaffd172c7a3894ab38aaa
Dan Schreiber <dmschreiber@gmail.com>
2023-12-15 20:51:10 -0500

    Day 16 prep

---
+++ day16.py
@@ -0,0 +1,29 @@
+import re
+import scrib as s
+import os
+from collections import namedtuple
+
+
+def solve(input):
+    with open(input) as f:
+        lines = f.read().splitlines()
+
+    p1, p2 = 0, 0
+
+    return p1, p2
+
+
+if __name__ == '__main__':
+    d = s.find_filename(__file__)
+    d = d[:len(d)-3]
+
+    input_file = "./data/" + d + "_input.txt"
+    p1, p2 = solve(input_file)
+    print("{} part 1: {}".format(d,p1))
+    print("{} part 2: {}".format(d,p2))
+
+    # lst = [1, 4, 4, 4, 2, 5, 6, 6, 7, 8, 9, 10]
+    # print(s.find_most_frequent(lst))
+    # print(s.find_occurances(lst)[4])
+    # print(s.find_even(lst))
+    # print(s.capitalize_words(["python", "javaScript", "c++"]))
